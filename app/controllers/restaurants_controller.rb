c9074aef35b103382dea7cc309180292758922b0
Lucy Treganna lucy.treganna@gmail.com <lucy.treganna@gmail.com>
2023-01-26 16:41:39 +0100

    refactored

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,7 @@ class RestaurantsController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:id])
+    @review = @restaurant.reviews.new
   end
 
   def new
@@ -13,8 +14,11 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to restaurant_path(@restaurant.id)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,14 @@
 class ReviewsController < ApplicationController
   before_action :set_restaurant
 
-  def new
-    @review = Review.new
-  end
-
   def create
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'restaurants/show', status: :unprocessable_entity
+    end
   end
 
   private
